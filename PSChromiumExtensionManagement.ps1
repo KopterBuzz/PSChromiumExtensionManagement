b3873d0d14479e68d54aa2c7132155009e6d3a09
Gabor Nemeth <etgabo@gmail.com>
2025-01-16 08:56:45 +0100

    removed globals

--- PSChromiumExtensionManagement.ps1
+++ PSChromiumExtensionManagement.ps1
@@ -13,7 +13,7 @@ If you want to contribute, fork the repo and raise a PR.
 #>
 
 #global variables to store state for supported browsers
-$Global:PSChromiumSupportedBrowsers = @{
+$PSChromiumSupportedBrowsers = @{
     "Google Chrome" = [PSCustomObject]@{
         Name = "Google Chrome"
         RegistryPath = "HKLM:\SOFTWARE\Policies\Google\Chrome"
@@ -40,24 +40,24 @@ $Global:PSChromiumSupportedBrowsers = @{
 }
 
 #evil genius trick to allow some PowerShell 5 cmdlets
-$Global:PSChromiumGetPackageSession = $null
-if($PSVersionTable.PSVersion.Major -eq 7) {$Global:PSChromiumGetPackageSession = New-PSSession -UseWindowsPowerShell}
+$PSChromiumGetPackageSession = $null
+if($PSVersionTable.PSVersion.Major -eq 7) {$PSChromiumGetPackageSession = New-PSSession -UseWindowsPowerShell}
 
 #list of available Extension Permissions
-$Global:PSChromiumSupportedExtensionPermissions = Get-Content $((get-location).path+"\ChromiumExtensionPermissions.txt")
+$PSChromiumSupportedExtensionPermissions = Get-Content $((get-location).path+"\ChromiumExtensionPermissions.txt")
 
 
 #to check if specific browser or browsers are installed
 function Confirm-PSChromiumBrowserIsInstalled {
     Param(
         [Parameter(Mandatory)]
-        [ValidateScript({$_ -in $Global:PSChromiumSupportedBrowsers.Keys})]
+        [ValidateScript({$_ -in $PSChromiumSupportedBrowsers.Keys})]
         [String[]]$BrowserName 
     )
     #Get-Package doesn't work the same way on PowerShell 5 and 7 so we have to do this to maintain compatibility with both
-    if ($null -ne $Global:PSChromiumGetPackageSession) {
+    if ($null -ne $PSChromiumGetPackageSession) {
         return ($null -ne (Invoke-Command -ScriptBlock {param($name)Get-Package $name -ErrorAction SilentlyContinue} -ArgumentList ($BrowserName) `
-                                          -Session $Global:PSChromiumGetPackageSession))
+                                          -Session $PSChromiumGetPackageSession))
     } else {
         return $null -ne $(Get-Package $BrowserName -ErrorAction SilentlyContinue)
     }
@@ -65,40 +65,40 @@ function Confirm-PSChromiumBrowserIsInstalled {
 }
 
 #run initial browser install check on module load to initialize state
-$Global:PSChromiumSupportedBrowsers.Keys | ForEach-Object {
-    $Global:PSChromiumSupportedBrowsers[$_].Installed = Confirm-PSChromiumBrowserIsInstalled -BrowserName $_
+$PSChromiumSupportedBrowsers.Keys | ForEach-Object {
+    $PSChromiumSupportedBrowsers[$_].Installed = Confirm-PSChromiumBrowserIsInstalled -BrowserName $_
 }
 
 #used to refresh the list of extension settings in the global state
 function Get-PSChromiumExtensionSettings {
 Param(
     [Parameter(Mandatory)]
-    [ValidateScript({$_ -in $Global:PSChromiumSupportedBrowsers.Keys})]
+    [ValidateScript({$_ -in $PSChromiumSupportedBrowsers.Keys})]
     [String[]]$BrowserName
     )
 
     foreach ($Browser in $BrowserName) {
         $Data = $null
-        $JSON = Get-ItemPropertyValue -Path $Global:PSChromiumSupportedBrowsers[$Browser].RegistryPath `
-                                      -Name $Global:PSChromiumSupportedBrowsers[$Browser].ExtensionSettingsName `
+        $JSON = Get-ItemPropertyValue -Path $PSChromiumSupportedBrowsers[$Browser].RegistryPath `
+                                      -Name $PSChromiumSupportedBrowsers[$Browser].ExtensionSettingsName `
                                       -ErrorAction SilentlyContinue
 
         if ($JSON) {
             $Data = $JSON | ConvertFrom-Json
-            $Global:PSChromiumSupportedBrowsers[$Browser].ExtensionSettings = $Data
+            $PSChromiumSupportedBrowsers[$Browser].ExtensionSettings = $Data
         }
 
     }
 }
 #update global state on load
-Get-PSChromiumExtensionSettings -BrowserName $Global:PSChromiumSupportedBrowsers.Keys
+Get-PSChromiumExtensionSettings -BrowserName $PSChromiumSupportedBrowsers.Keys
 
 #install or update configuration for an extension
 #you can specify a single or multiple browsers at once
 function Set-PSChromiumExtension {
     Param(
         [Parameter(Mandatory)]
-        [ValidateScript({$_ -in $Global:PSChromiumSupportedBrowsers.Keys})]
+        [ValidateScript({$_ -in $PSChromiumSupportedBrowsers.Keys})]
         [String[]]$BrowserName,
         [Parameter(Mandatory)]
         [String]$ExtensionID,
@@ -106,7 +106,7 @@ function Set-PSChromiumExtension {
         [ValidateScript({$_.length -le 1000})]
         [String]$BlockedInstallMessage,
         [Parameter()]
-        [ValidateScript({$_ -in $Global:PSChromiumSupportedExtensionPermissions})]
+        [ValidateScript({$_ -in $PSChromiumSupportedExtensionPermissions})]
         [String]$BlockedPermissions,
         [Parameter()]
         [ValidateSet("True","False")]
@@ -170,53 +170,53 @@ function Set-PSChromiumExtension {
 
     #inject extension setting into the specified browser(s) ExtensionSettings JSON, create the regstry paths if necessary
     foreach ($Browser in $BrowserName) {
-        if (!$Global:PSChromiumSupportedBrowsers[$Browser].Installed) {
+        if (!$PSChromiumSupportedBrowsers[$Browser].Installed) {
             Write-Error $("$Browser Is Not Installed. Will not install extension.")
             Continue
         }
         try {
-            Get-Iitem -Path $Global:PSChromiumSupportedBrowsers[$Browser].RegistryPath
+            Get-Iitem -Path $PSChromiumSupportedBrowsers[$Browser].RegistryPath
         } catch {
-            New-Item -Path $Global:PSChromiumSupportedBrowsers[$Browser].RegistryPath -Force
+            New-Item -Path $PSChromiumSupportedBrowsers[$Browser].RegistryPath -Force
         }
 
-        if ($null -eq $Global:PSChromiumSupportedBrowsers[$Browser].ExtensionSettings) {
-            New-ItemProperty -Path $Global:PSChromiumSupportedBrowsers[$Browser].RegistryPath `
-                             -Name $Global:PSChromiumSupportedBrowsers[$Browser].ExtensionSettingsName `
+        if ($null -eq $PSChromiumSupportedBrowsers[$Browser].ExtensionSettings) {
+            New-ItemProperty -Path $PSChromiumSupportedBrowsers[$Browser].RegistryPath `
+                             -Name $PSChromiumSupportedBrowsers[$Browser].ExtensionSettingsName `
                              -Value ($ExtensionSettingsObject | ConvertTo-Json)
-            Get-PSChromiumExtensionSettings -BrowserName $Global:PSChromiumSupportedBrowsers.Keys
+            Get-PSChromiumExtensionSettings -BrowserName $PSChromiumSupportedBrowsers.Keys
             return
         } 
 
-        if ($Global:PSChromiumSupportedBrowsers[$Browser].ExtensionSettings.$ExtensionID) {
-            $Global:PSChromiumSupportedBrowsers[$Browser].ExtensionSettings.$ExtensionID = $ExtensionSettingsObject.$ExtensionID
+        if ($PSChromiumSupportedBrowsers[$Browser].ExtensionSettings.$ExtensionID) {
+            $PSChromiumSupportedBrowsers[$Browser].ExtensionSettings.$ExtensionID = $ExtensionSettingsObject.$ExtensionID
         } else {
-            $Global:PSChromiumSupportedBrowsers[$Browser].ExtensionSettings | Add-Member -MemberType NoteProperty `
+            $PSChromiumSupportedBrowsers[$Browser].ExtensionSettings | Add-Member -MemberType NoteProperty `
                                                     -Name $ExtensionID `
                                                     -Value $ExtensionSettingsObject.$ExtensionID -Force
         }
-        Set-ItemProperty -Path $Global:PSChromiumSupportedBrowsers[$Browser].RegistryPath `
-                         -Name $Global:PSChromiumSupportedBrowsers[$Browser].ExtensionSettingsName `
-                         -Value ($Global:PSChromiumSupportedBrowsers[$Browser].ExtensionSettings | ConvertTo-Json) -Force
+        Set-ItemProperty -Path $PSChromiumSupportedBrowsers[$Browser].RegistryPath `
+                         -Name $PSChromiumSupportedBrowsers[$Browser].ExtensionSettingsName `
+                         -Value ($PSChromiumSupportedBrowsers[$Browser].ExtensionSettings | ConvertTo-Json) -Force
 
     }
-    Get-PSChromiumExtensionSettings -BrowserName $Global:PSChromiumSupportedBrowsers.Keys
+    Get-PSChromiumExtensionSettings -BrowserName $PSChromiumSupportedBrowsers.Keys
 }
 
 function Remove-PSChromiumExtension {
     Param(
         [Parameter(Mandatory)]
-        [ValidateScript({$_ -in $Global:PSChromiumSupportedBrowsers.Keys})]
+        [ValidateScript({$_ -in $PSChromiumSupportedBrowsers.Keys})]
         [String[]]$BrowserName,
         [Parameter(Mandatory)]
         [String[]]$ExtensionID
     )
     foreach ($Browser in $BrowserName) {
-        if ($Global:PSChromiumSupportedBrowsers[$Browser].ExtensionSettings.$ExtensionID) {
-            Set-ItemProperty -Path $Global:PSChromiumSupportedBrowsers[$Browser].RegistryPath `
-            -Name $Global:PSChromiumSupportedBrowsers[$Browser].ExtensionSettingsName `
-            -Value ($Global:PSChromiumSupportedBrowsers[$Browser].ExtensionSettings.PSObject.Properties.Remove($ExtensionID) | ConvertTo-Json) -Force
+        if ($PSChromiumSupportedBrowsers[$Browser].ExtensionSettings.$ExtensionID) {
+            Set-ItemProperty -Path $PSChromiumSupportedBrowsers[$Browser].RegistryPath `
+            -Name $PSChromiumSupportedBrowsers[$Browser].ExtensionSettingsName `
+            -Value ($PSChromiumSupportedBrowsers[$Browser].ExtensionSettings.PSObject.Properties.Remove($ExtensionID) | ConvertTo-Json) -Force
         }
     }
-    Get-PSChromiumExtensionSettings -BrowserName $Global:PSChromiumSupportedBrowsers.Keys
+    Get-PSChromiumExtensionSettings -BrowserName $PSChromiumSupportedBrowsers.Keys
 }
